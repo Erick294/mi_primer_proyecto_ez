13a8323d5e172802a9cd8ffe7ae52157b62a9b8e
Erick Zambrano <beowulfblackman@hotmail.com>
2022-06-09 14:52:36 -0500

    Tarea6

--- js/mis_primeros_scripts.js
+++ js/mis_primeros_scripts.js
@@ -1,21 +1,34 @@
 function ocultarMostrarImagen(){
     var estadoActualImagen = document.getElementById('idImagen').style.display;
     if(estadoActualImagen == 'none'){
-        estadoActualImagen = 'block'
+        estadoActualImagen = 'block';
     }else{
-        estadoActualImagen = 'none'
+        estadoActualImagen = 'none';
     }
  }
 
  function enciende(){
      var valorSRC = document.getElementById('idFoco').src;
      console.log(valorSRC);
-     document.getElementById('idFoco').src="../img/encendido.png"
+     document.getElementById('idFoco').src="../img/encendido.png";
  }
 
  function apagar(){
     var valorSRC = document.getElementById('idFoco').src;
     console.log(valorSRC);
-    document.getElementById('idFoco').src="../img/apagado.png"
+    document.getElementById('idFoco').src="../img/apagado.png";
 
+ }
+
+ function encenderApagar(){
+     var estadoActual = document.getElementById('idFoco').className;
+     var srcApagado = "../img/apagado.png";
+     var srcEncendido= "../img/encendido.png";
+     if(estadoActual == 'apagado'){
+        document.getElementById('idFoco').src = srcEncendido;
+        document.getElementById('idFoco').className = 'encendido';
+     }else{
+        document.getElementById('idFoco').src = srcApagado;
+        document.getElementById('idFoco').className = 'apagado';
+     }
  }
